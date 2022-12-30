04dbeaa401f0de7e1df64200321b468ba057f891
N-None <bcsf21m004@pucit.edu.pk>
2022-12-30 22:49:06 +0500

    first commit

---
+++ problem1.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int main(){
+int a = 1;
+int b = 10;
+int c = 100;
+int x = 0;
+x = c * 3;
+x = x + (b * 6);
+x = x * 5;
+cout<<"The value of x = "<<x;
+
+
+    return 0;
+}
